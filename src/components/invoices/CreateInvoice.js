edd344ad032bda479a747db32303207286a70205
Alex Vyatkin <alexander.vjatkin@gmail.com>
2017-03-20 01:08:24 +0300

    not yet final commit

--- src/components/invoices/CreateInvoice.js
+++ src/components/invoices/CreateInvoice.js
@@ -1,23 +1,103 @@
 import React, { PureComponent } from 'react';
 import axios from 'axios';
 import { Form, FormGroup, FormControl, Button, Col, Table } from 'react-bootstrap';
+import { Link, browserHistory } from 'react-router';
 
 export default class CreateInvoice extends PureComponent {
   constructor() {
     super();
-    this.state = { data: [] };
+    this.state = {
+      id: null,
+      data: {
+        nextIndexForIterateElement: 1,
+        source: [],
+      },
+      total: 0,
+    };
+
     this.addProduct = () => {
       const select = document.getElementById('product');
       const selectedProductId = select.options[select.selectedIndex].value;
-      console.log(selectedProductId);
       axios.get(`/api/products/${selectedProductId}`)
         .then((res) => {
-          console.log(res);
-          const newData = [...this.state.data, res.data];
+          const newData = [...this.state.data.source, {
+            itemId: this.state.data.nextIndexForIterateElement,
+            itemData: res.data,
+            qty: 1,
+          }];
           this.setState({
-            data: newData,
+            data: {
+              nextIndexForIterateElement: this.state.data.nextIndexForIterateElement + 1,
+              source: newData,
+            },
           });
         });
+    };
+
+    this.addProductQuantity = (e) => {
+      const productId = Number(e.target.dataset.id);
+      const newSource = this.state.data.source.map((item) => {
+        const newItem = (item.itemId === productId) ?
+          { itemId: item.itemId, itemData: item.itemData, qty: Number(e.target.value) } :
+          item;
+        return newItem;
+      });
+      this.setState({
+        data: {
+          nextIndexForIterateElement: this.state.data.nextIndexForIterateElement,
+          source: newSource,
+        },
+      });
+    };
+
+    this.removeProduct = (e) => {
+      const removeID = Number(e.target.dataset.id);
+      const newSource = this.state.data.source.filter(item => item.itemId !== removeID);
+      this.setState({
+        data: {
+          nextIndexForIterateElement: this.state.data.nextIndexForIterateElement,
+          source: newSource,
+        },
+      });
+    };
+
+    this.getTotal = () => {
+      const price = this.state.data.source
+        .reduce((acc, item) => acc + (item.qty * item.itemData.price), 0);
+      const discount = Number(document.getElementById('discount').value);
+      const total = Math.round(price * (100 - discount)) / 100;
+      this.setState({ total, discount });
+    };
+
+    this.sendData = (e) => {
+      e.preventDefault();
+      if (!this.state.id) {
+        this.createNewInvoice();
+      } else {
+        this.updateInvoice();
+      }
+    };
+
+    this.createNewInvoice = () => {
+      const sendInvoiceData = {
+        customer_id: this.state.currentCustomer,
+        discount: this.state.discount,
+        total: this.state.total,
+      };
+      axios.post('/api/invoices', sendInvoiceData)
+        .then((res) => {
+          this.state.data.source.map((item) => {
+            axios.post(`/api/invoices/${res.data.id}/items`, {
+              product_id: item.itemData.id,
+              quantity: item.qty,
+            }).then(() => browserHistory.push('/invoices'));
+            return item;
+          });
+        });
+    };
+
+    this.updateInvoice = () => {
+
     };
   }
 
@@ -25,6 +105,7 @@ export default class CreateInvoice extends PureComponent {
     axios.get('/api/customers')
       .then(res => this.setState({
         customers: res.data,
+        currentCustomer: res.data[0].id,
       }));
     axios.get('/api/products')
       .then(res => this.setState({
@@ -32,21 +113,25 @@ export default class CreateInvoice extends PureComponent {
       }));
   }
 
+  componentDidUpdate() {
+    this.getTotal();
+  }
+
   render() {
     return (
       <div >
         <h1>Create invoice</h1>
-        <Form horizontal>
+        <Form horizontal onSubmit={this.sendData}>
           <FormGroup>
             <Col sm={2}>
               <label htmlFor='discount'>Discount (%)</label>
-              <FormControl type='number' id='discount' />
+              <FormControl type='number' id='discount' defaultValue='0' onChange={this.getTotal} />
             </Col>
           </FormGroup>
           <FormGroup>
             <Col sm={4}>
               <label htmlFor='customer'>Customer</label>
-              <FormControl componentClass='select' id='customer'>
+              <FormControl componentClass='select' id='customer' onChange={(e) => { this.setState({ currentCustomer: Number(e.target.value) }); }}>
                 {(this.state.customers) ?
                     this.state.customers.map((item) => {
                       return (
@@ -74,19 +159,35 @@ export default class CreateInvoice extends PureComponent {
             </Col>
             <Button onClick={this.addProduct} >Add</Button>
           </FormGroup>
+          <Table responsive className='invoiceProductTable'>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Price</th>
+                <th>Qty</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.state.data.source.map(item => (
+                <tr key={item.itemId} id={item.itemId} className='invoiceProduct'>
+                  <td>{item.itemData.name}</td>
+                  <td id={`price-${item.itemId}`}>{item.itemData.price}</td>
+                  <td id={`qty-${item.itemId}`}>
+                    <FormControl type='number' defaultValue='1' onChange={this.addProductQuantity} data-id={item.itemId} />
+                  </td>
+                  <td><Button bsStyle='danger' onClick={this.removeProduct} data-id={item.itemId}>Delete</Button></td>
+                </tr>))}
+            </tbody>
+          </Table>
+          <h1>Total: {this.state.total}</h1>
+          <Col sm={11}>
+            <Button type='submit' bsStyle='info'>Submit</Button>
+          </Col>
+          <Col sm={1}>
+            <Link to='/invoices' className='btn btn-default'>Cancel</Link>
+          </Col>
         </Form>
-        <Table responsive>
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Price</th>
-              <th>Quantity</th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.state.data.map((item, index) => <tr key={index}><td>{item.name}</td><td>{item.price}</td><td><FormControl type='number' /></td></tr>)}
-          </tbody>
-        </Table>
       </div>
     );
   }
--- src/components/invoices/Invoice.js
+++ src/components/invoices/Invoice.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import { Link } from 'react-router';
+import { Link, browserHistory } from 'react-router';
+import axios from 'axios';
 import TemplateComponent from '../templates/TemplateComponent';
 
 export default class InvoiceList extends TemplateComponent {
   constructor() {
     super();
     this.state = {
+      data: [],
       name: 'Invoice',
       columns: {
         1: '#',
@@ -17,30 +19,35 @@ export default class InvoiceList extends TemplateComponent {
       body: () => {
         return null;
       },
+      url: '/api/invoices',
     };
-    this.createOnClick = () => {
-
+    this.getData = () => {
+      axios(this.state.url)
+      .then(res => res.data)
+      .then((res) => {
+        res.map((item, index) => {
+          axios.get(`/api/customers/${item.customer_id}`).then(resolve => resolve.data).then((r) => {
+            const dataItem = {
+              idx: index + 1,
+              customer: r.name,
+              discount: item.discount,
+              total: item.total,
+            };
+            return dataItem;
+          }).then((r) => {
+            this.setState({ data: this.state.data.concat([{ id: item.id, data: r }]) });
+          });
+          return item;
+        });
+      });
     };
     this.createButton = <Link className='btn btn-default create' to='/invoices/create'>Create</Link>;
+    this.delete = () => {
+      axios.delete(`/api/invoices/${this.state.idToDelete}`).then(() => this.setState({ showConfirmModal: false })).then(() => browserHistory.push('/invoices'));
+    };
   }
 
   componentWillMount() {
-    fetch('/api/invoices')
-      .then(res => res)
-      .then((res) => {
-        const data = (data) && res.reduce((acc, item, index) => {
-          return acc.concat({
-            id: data.id,
-            data: {
-              idx: index + 1,
-              id: item.id,
-              customer_id: item.customer_id,
-              disount: item.discount,
-              total: item.total,
-            },
-          });
-        }, []);
-        this.setState({ data });
-      });
+    this.getData();
   }
 }
